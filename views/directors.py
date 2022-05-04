dd038a744d2cbf43cadab562e941273b60ed0457
kostin <kostin>
2022-05-05 00:08:10 +0300

    Added Authorized Required + Role admin Required directors, genres, movies methods POST, PUT, DELETE

--- views/directors.py
+++ views/directors.py
@@ -2,17 +2,21 @@ from flask_restx import Resource, Namespace
 from flask import request
 from dao.model.director import DirectorSchema
 from implemented import director_service
+from utils import auth_required, admin_access_required
 
 director_ns = Namespace('directors')
 
 
 @director_ns.route('/')
 class DirectorsView(Resource):
+    @auth_required
     def get(self):
         rs = director_service.get_all()
         res = DirectorSchema(many=True).dump(rs)
         return res, 200
 
+    @auth_required
+    @admin_access_required
     def post(self):
         req_json = request.json
         director = director_service.create(req_json)
@@ -21,11 +25,14 @@ class DirectorsView(Resource):
 
 @director_ns.route('/<int:rid>')
 class DirectorView(Resource):
+    @auth_required
     def get(self, rid):
         r = director_service.get_one(rid)
         sm_d = DirectorSchema().dump(r)
         return sm_d, 200
 
+    @auth_required
+    @admin_access_required
     def put(self, rid):
         req_json = request.json
         if "id" not in req_json:
@@ -33,6 +40,8 @@ class DirectorView(Resource):
         director_service.update(req_json)
         return "", 204
 
+    @auth_required
+    @admin_access_required
     def delete(self, rid):
         director_service.delete(rid)
         return "", 204
